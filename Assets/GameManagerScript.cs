406a472824b8ad58aa42d52acf04b011dd288c7d
Nigibob <nelsonat@laposte.net>
2019-12-09 19:48:35 +0100

    Initial commit

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    [SerializeField]
+
+    private GameObject ImWin;
+
+    public void WinGame()
+    {
+        ImWin.SetActive(true);
+    }
+
+}
